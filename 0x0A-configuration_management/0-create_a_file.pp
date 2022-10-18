170c6736542275acb202141162d6b6d8bfb26a7c
boa20 <kwesikyeremateng5@gmail.com>
2022-10-18 07:12:32 -0700

    Puppet file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 file { '/tmp/school':
-  ensure => file,
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
